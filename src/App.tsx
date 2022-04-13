cde68922a3781c1d4b2d7aa360c0f69684cf3d3e
Richard Lu <richardblu37@gmail.com>
2022-04-13 18:19:05 -0400

    refine channel preview tooltips, add channel data

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,27 @@ function App() {
       <Navbar />
       <main>
         <Sidebar />
+        <section>
+          <h2>Live channels we think you'll like</h2>
+          <div className="thumbnail-container">
+            <img src="" alt="" />
+            <span>Live</span>
+            <span>19.4K viewers</span>
+          </div>
+          <div className="channel-info">
+            <div className="profile-image-container">
+              <img src="" alt="" />
+            </div>
+            <div className="channel-info">
+              <h3>Title Goes Here</h3>
+              <span>loltyler1</span>
+              <span>League of Legends</span>
+              <div className='tags'>
+                <span>English</span>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
